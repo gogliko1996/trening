db4d348889df34930329eb97acd9c9cae34ed1b1
gogliko1996 <chakiduli@gmail.com>
2022-09-21 20:38:58 +0400

    trening

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// let user = {  
+//      firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus:"action",
+// } 
+// if(user.age<18&user.studentstatus ==="action"){
+//     console.log("helo");
+// }
+// else{
+//     console.log("helo");
+// }
+
+
+
+// let numbers=[5,6,10,25,3,7,4,100,28,36,89];
+// for(let i=0; i<numbers.length; i++){
+//     if(numbers[i]>5){
+//         console.log(numbers[i]);
+//     }
+// }
+
+// let array =[ "watermelon", "pear", 10, 45, 50, "apple", "ananas"];
+// for(let i=0; i<array.length; i++){
+//     if(typeof array[i] !== "number"){
+//         console.log(array[i]);
+//     }
+// }
+let array = [[2, -3, 5, 10],[25,-24, -11, 100],[-6, -7, 10]];
+for(let i=0; i<array.length; i++){
+   if(array[i][0]>0){
+    console.log(array[i] [0]);
+
+   }
+
