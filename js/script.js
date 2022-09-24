3b106d172a4e65d75e8ecd5af96eddb4f88ed280
gogliko1996 <chakiduli@gmail.com>
2022-09-24 17:48:09 +0400

    gaga

--- js/script.js
+++ js/script.js
@@ -1,54 +1,81 @@
-// let user = {  
-//      firstname: "giorgi",
-//     lastname: "smith",
-//     age: 25,
-//     studentstatus:"action",
-// } 
-// if(user.age<18&user.studentstatus ==="action"){
-//     console.log("helo");
+
+
+
+
+
+// let r=[1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// let a=0
+// for(let i=0; i<r.length; i++){
+//     a+=r[i]
+// //     if(r[i] === 5)
+// //    r.splice(i, 3);
 // }
-// else{
-//     console.log("helo");
+// console.log(a);
+
+// function user(...array){
+//     let a=0;
+//     for(let i=0; i<array.length; i++){
+//       if(array[i] > 0) {
+//         a+=array[i];
+//       }
+//        console.log(a);
+//     }
+// }
+//  user(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+
+
+
+// function user(...array){
+//     a=0;
+//     for(let i=0; i<array.length; i++){
+//         a+=array[i];
+//     }
+//     console.log(a);
+// }
+// user(10, 50, 6, 7, 8, 11, 6, 3, 9);
+
+
+
+//     let user = {
+//         firstname: 'giorgi',
+//         lastname: 'saakadze',
+//         age: 32,
+//         isloggedin: true
+//       }
+   
+// function object1 (a, b) {
+//      return a + " " + b;
+     
+//  }
+//  let username = object1(user.firstname, user.lastname);
+//  console.log(username);
+
+
+
+// function user(...array){
+//     b=0
+//     for ( let i=0; i<array.length; i++){
+//         if(b < array[i]){
+//             b=array[i];
+//         }
+//     }
+//     return b; 
+// }
+// let a = user(1,2,4,10,34,5,70,6)
+// console.log(a);
+
+
+// let array = [1,2,4,10,34,5,7,87];
+// for(let i=0; i<array.length; i++){
+//     if(0<array[i] & 10>array[i]){
+//         console.log(array[i]);
+//     }
 // }
 
 
-
-// let numbers=[5,6,10,25,3,7,4,100,28,36,89];
-// for(let i=0; i<numbers.length; i++){
-//     if(numbers[i]>5){
+// let numbers =[1,2,3,4,5,6,7,8,9,10];
+// for(let i=0; i< numbers.length; i++){
+//     if(5 == numbers[i]){
 //         console.log(numbers[i]);
 //     }
 // }
-
-// let array =[ "watermelon", "pear", 10, 45, 50, "apple", "ananas"];
-// for(let i=0; i<array.length; i++){
-//     if(typeof array[i] !== "number"){
-//         console.log(array[i]);
-//     }
-// }
-
-// let array = [
-//     [2, -3, 5, 10],
-//     [25,-24, -11, 100],
-//     [-6, -7, 10]
-// ];
-// for(let i=0; i<array.length; i++){
-//     for(let a=0; a<array[i].length; a++){
-//         if(array[i][a] > 0){
-//             console.log(array[i][a]);
-//         }
-//     }
-// }
-// let array = [2, 3, 5, 10, 25, 24, 11, 100, 6, 7, 10];
-// for(let i=0; i<array.length; i++){
-//     if(array[i] % 2 == 0){
-//         console.log(array[i]);
-//     }
-// }
-
-let user =[
-    {username:"giorgi", status:"falce"},
-    {username:"levani", status:"falce"},
-    {username:"anna", status:"true"}
-]
-console.log(user[2].status);
