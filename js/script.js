3075ad63c9cac431e36894405cb63fac2f0b1884
gogliko1996 <chakiduli@gmail.com>
2022-09-29 22:19:57 +0400

    sasa

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 
-document.getElementById("line1").appendChild(document.createElement("div"));
+document.getElementById("line2").appendChild(document.createElement("div"));
 
  document.getElementById("lineBox").addEventListener("click", function() { 
     document.getElementById("lineBox").classList.toggle("lineBoxClic");
-    document.getElementById("line1").classList.toggle("lineX");
-    document.getElementById("line2").classList.toggle("lineNone");
+    document.getElementById("line2").classList.toggle("lineX");
+    document.getElementById("line1").classList.toggle("lineNone");
     document.getElementById("line3").classList.toggle("lineNone");
  });
 
