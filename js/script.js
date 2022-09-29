ec9337c1499eeea0fd8bb043a4b9c788e7b51230
gogliko1996 <chakiduli@gmail.com>
2022-09-29 22:06:16 +0400

    burger

--- js/script.js
+++ js/script.js
@@ -1,87 +1,18 @@
 
+document.getElementById("line1").appendChild(document.createElement("div"));
+
+ document.getElementById("lineBox").addEventListener("click", function() { 
+    document.getElementById("lineBox").classList.toggle("lineBoxClic");
+    document.getElementById("line1").classList.toggle("lineX");
+    document.getElementById("line2").classList.toggle("lineNone");
+    document.getElementById("line3").classList.toggle("lineNone");
+ });
 
 
 
 
-// let r=[1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-// let a=0
-// for(let i=0; i<r.length; i++){
-//     a+=r[i]
-// //     if(r[i] === 5)
-// //    r.splice(i, 3);
-// }
-// console.log(a);
 
-// function user(...array){
-//     let a=0;
-//     for(let i=0; i<array.length; i++){
-//       if(array[i] > 0) {
-//         a+=array[i];
-//       }
-//        console.log(a);
-//     }
-// }
-//  user(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
-
-
-
-// function user(...array){
-//     a=0;
-//     for(let i=0; i<array.length; i++){
-//         a+=array[i];
-//     }
-//     console.log(a);
-// }
-// user(10, 50, 6, 7, 8, 11, 6, 3, 9);
-
-
-
-    let user = {
-        firstname: 'giorgi',
-        lastname: 'saakadze',
-        age: 32,
-        isloggedin: true
-      }
-   
-function getUsername (a, b) {
-      return a + " " + b;
-     
- }
- let username = getUsername(user.firstname, user.lastname);
- console.log(username);
-
-
-
-// function user(...array){
-//     b=0
-//     for ( let i=0; i<array.length; i++){
-//         if(b < array[i]){
-//             b=array[i];
-//         }
-//     }
-//     return b; 
-// }
-// let a = user(1,2,4,10,34,5,70,6)
-// console.log(a);
-
-
-// let array = [1,2,4,10,34,5,7,87];
-// for(let i=0; i<array.length; i++){
-//     if(0<array[i] & 10>array[i]){
-//         console.log(array[i]);
-//     }
-// }
-
-
-// let numbers =[1,2,3,4,5,6,7,8,9,10];
-// for(let i=0; i< numbers.length; i++){
-//     if(5 == numbers[i]){
-//         console.log(numbers[i]);
-//     }
-// }
-
-
-
+    
 
 
 
